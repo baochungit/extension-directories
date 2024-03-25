f2a7dce0a222944a87046be0d91666fdbb6d0dd1
Chung Xa <wall85vn@yahoo.com>
2024-03-25 14:44:22 +0700

    Update extension_android.cpp

--- directories/src/extension_android.cpp
+++ directories/src/extension_android.cpp
@@ -8,7 +8,8 @@ typedef enum {
 	system_directory_caches,
 	system_directory_documents,
 	system_directory_resource,
-	system_directory_temporary
+	system_directory_temporary,
+	system_directory_no_backup,
 }   SystemDirectory;
 
 static int path_for_file(lua_State *L) {
@@ -28,6 +29,7 @@ static int path_for_file(lua_State *L) {
 	jmethodID activity_get_package_resource_path = env->GetMethodID(activity_class, "getPackageResourcePath", "()Ljava/lang/String;");
 	jmethodID activity_get_files_dir = env->GetMethodID(activity_class, "getFilesDir", "()Ljava/io/File;");
 	jmethodID activity_get_cache_dir = env->GetMethodID(activity_class, "getCacheDir", "()Ljava/io/File;");
+	jmethodID activity_get_no_backup_dir = env->GetMethodID(activity_class, "getNoBackupFilesDir", "()Ljava/io/File;");
 
 	int count = lua_gettop(L);
 	const char *file_path = NULL;
@@ -63,6 +65,12 @@ static int path_for_file(lua_State *L) {
 		case system_directory_documents:
 			{
 				jobject files_file_object = env->CallObjectMethod(dmGraphics::GetNativeAndroidActivity(), activity_get_files_dir);
+				directory_path_string = (jstring)env->CallObjectMethod(files_file_object, file_get_canonical_path);
+			}
+			break;
+		case system_directory_no_backup:
+			{
+				jobject files_file_object = env->CallObjectMethod(dmGraphics::GetNativeAndroidActivity(), activity_get_no_backup_dir);
 				directory_path_string = (jstring)env->CallObjectMethod(files_file_object, file_get_canonical_path);
 			}
 			break;
@@ -118,6 +126,9 @@ dmExtension::Result INITIALIZE(dmExtension::Params *params) {
 
 	lua_pushnumber(L, system_directory_temporary);
 	lua_setfield(L, -2, "temporary");
+
+	lua_pushnumber(L, system_directory_no_backup);
+	lua_setfield(L, -2, "no_backup");
 
 	return dmExtension::RESULT_OK;
 }
